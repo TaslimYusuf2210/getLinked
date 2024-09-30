de4a0d79aa3ee00002d70a775090ed4564f90f16
Taslim Omotoyosi Yusuf <112773318+TaslimYusuf2210@users.noreply.github.com>
2024-09-30 20:08:15 +0100

    form validation

--- src/components/pages/contact.jsx
+++ src/components/pages/contact.jsx
@@ -2,43 +2,109 @@ import { FaFacebookF } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  teamsName: yup.string().required("Please enter the team's name"),
+  topic: yup.string().required("This field is required"),
+  email: yup
+    .string()
+    .email("Invalid email address")
+    .required("Email address is required"),
+  message: yup.string().required("This field is required"),
+});
 
 function Contact() {
-  return ( 
-    <div className="font-montserrat grid gap-20 place-items-center text-left px-16">
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    console.log("form submitted"); // Handle form data
+  };
+
+  return (
+    <div className="font-montserrat grid gap-10 place-items-center text-left px-16">
       <div className="w-full ">
         <p className="text-xl leading-6 text-customPurple font-clashDisplay mt-10">
-        Questions or need <br />
-        assistance? <br />
-        Let us know about it
+          Questions or need <br />
+          assistance? <br />
+          Let us know about it
         </p>
         <p className="text-xs mt-8">
-        Email us below to any question related <br />
-        to our event
+          Email us below to any question related <br />
+          to our event
         </p>
       </div>
       <div className="w-full text-center">
-        <form className="grid grid-cols-1 gap-6 w-full" action="">
-          <input className="input-border placeholder-white" placeholder="Team's Name" type="text" />
-          <input className="input-border placeholder-white" placeholder="Topic" type="text" />
-          <input className="input-border placeholder-white" placeholder="Email" type="email" />
-          <input className="input-border placeholder-white" placeholder="Message" type="textarea" />
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="grid grid-cols-1 gap-6 w-full"
+          action=""
+        >
+          <div className="text-left">
+            <input
+              className="input-border px-6 text-base placeholder-white"
+              placeholder="Team's Name"
+              type="text"
+              {...register("teamsName")}
+            />
+            <p className="text-xs text-red-500">{errors.teamsName?.message}</p>
+          </div>
+
+          <div className="text-left">
+            <input
+              className="input-border px-6 placeholder-white"
+              placeholder="Topic"
+              type="text"
+              {...register("topic")}
+            />
+            <p className="text-xs text-red-500">{errors.topic?.message}</p>
+          </div>
+
+          <div className="text-left">
+            <input
+              className="input-border px-6 placeholder-white"
+              placeholder="Email"
+              type="email"
+              {...register("email")}
+            />
+            <p className="text-xs text-red-500">{errors.email?.message}</p>
+          </div>
+
+          <div className="text-left">
+            <textarea
+              className="input-border h-40 px-6 pt-4 align-text-top placeholder-white"
+              placeholder="Message"
+              type="text"
+              {...register("message")}
+            />
+            <p className="text-xs text-red-500">{errors.message?.message}</p>
+          </div>
+
           <div className="w-full">
-          <button className="button w-44 py-[18px]">Submit</button>
+            <button className="button w-44 py-[18px]">Submit</button>
           </div>
         </form>
       </div>
       <div className="text-center flex flex-col gap-4 ">
         <p className="text-customPurple text-xs">Share on</p>
         <div className="flex gap-2 text-sm">
-        <FaInstagram />
-            <FaXTwitter />
-            <FaFacebookF />
-            <FaLinkedinIn />
+          <FaInstagram />
+          <FaXTwitter />
+          <FaFacebookF />
+          <FaLinkedinIn />
         </div>
       </div>
     </div>
-   );
+  );
 }
 
 export default Contact;
--- src/components/pages/registration.jsx
+++ src/components/pages/registration.jsx
@@ -1,61 +1,156 @@
-import movement from "../../assets/movement.svg"
-import designer from "../../assets/designer.svg"
+import movement from "../../assets/movement.svg";
+import designer from "../../assets/designer.svg";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  teamsName: yup.string().required("Name is required"),
+  phoneNumber: yup
+    .string()
+    .matches(/^[0-9]{10}$/, "Phone number must be 10 digits")
+    .required("Phone number is required"),
+  emailAddress: yup
+    .string()
+    .email("Invalid email address")
+    .required("Email address is required"),
+  projectTopic: yup.string().required("Topic is required"),
+  category: yup.string().required("Select a category"),
+  groupSize: yup.string().required("Choose your group size"),
+  agree: yup.string().required("This field is required"),
+});
 
 function Registration() {
-  return ( 
+
+  const { 
+    register, 
+    handleSubmit, 
+    formState: { errors } 
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onTouched",
+  });
+
+  const onSubmit = ( data) => {
+    console.log(data)
+    console.log("form submitted"); // Handle form data
+  };
+
+  return (
     <div className="font-montserrat py-6 px-16">
-      <div className="text-left"> 
-        <h6 className="text-customPurple font-clashDisplay font-bold">Register</h6>
+      <div className="text-left">
+        <h6 className="text-customPurple font-clashDisplay font-bold">
+          Register
+        </h6>
       </div>
       <div className="grid place-items-center pt-8">
         <img src={designer} alt="designer" />
         <img src={movement} alt="movement" />
       </div>
       <div className="w-full">
-        <h2 className="text-xl w-full tracking-[0.015em]">CREATE YOUR ACCOUNT</h2>
-        <form action="">
+        <h2 className="text-xl w-full tracking-[0.015em]">
+          CREATE YOUR ACCOUNT
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)} action="">
           <div className="w-full grid grid-cols-1 gap-4 mt-4">
             <div className="w-full text-[13px]">
               <label htmlFor="">Team's Name</label>
-              <input className="input-border text-[13px] px-6" placeholder="Enter the name of your group"  type="text" />
+              <input
+                className="input-border text-[14px] px-6"
+                placeholder="Enter the name of your group"
+                type="text"
+                {...register("teamsName")}
+              />
+              <p className="text-xs text-red-500">{errors.teamsName?.message}</p>
             </div>
             <div className="w-full text-[13px]">
               <label htmlFor="">Phone</label>
-              <input className="input-border text-[13px] px-6" placeholder="Enter your phone number"  type="text" />
+              <input
+                className="input-border text-[14px] px-6"
+                placeholder="Enter your phone number"
+                type="number"
+                {...register("phoneNumber")}
+              />
+              <p className="text-xs text-red-500">{errors.phoneNumber?.message}</p>
             </div>
             <div className="w-full text-[13px]">
-              <label htmlFor="">Team's Name</label>
-              <input className="input-border text-[13px] px-6" placeholder="Enter your email address"  type="text" />
+              <label htmlFor="">Email Address</label>
+              <input
+                className="input-border text-[14px] px-6"
+                placeholder="Enter your email address"
+                type="text"
+                {...register("emailAddress")}
+              />
+              <p className="text-xs text-red-500">{errors.emailAddress?.message}</p> 
             </div>
             <div className="w-full text-[13px]">
-              <label htmlFor="">Team's Name</label>
-              <input className="input-border text-[13px] px-6" placeholder="What is your prohect topic"  type="text" />
+              <label htmlFor="">Project Topic</label>
+              <input
+                className="input-border text-[14px] px-6"
+                placeholder="What is your project topic"
+                type="text"
+                {...register("projectTopic")}
+              />
+              <p className="text-xs text-red-500">{errors.projectTopic?.message}</p> 
             </div>
           </div>
           <div className="w-full grid grid-cols-2 gap-2 mt-4">
-            <div className="w-full text-[13px] grid grid-col-1">
+            <div className=" text-[13px] grid grid-col-1 w-full">
               <label htmlFor="">Category</label>
-              <select className="bg-customPurple-darker input-border" name="" id="">
-                <option value="" disabled>Select your category</option>
+              <select
+                className=" input-border  text-white"
+                name=""
+                id=""
+                {...register("category")}
+              >
+                <option value="" selected disabled>
+                  Select your category
+                </option>
+                <option value="">
+                  category
+                </option>
               </select>
+              <p className="text-xs text-red-500">{errors.category?.message}</p> 
+
             </div>
             <div className="w-full text-[13px] grid grid-col-1">
               <label htmlFor="">Group Size</label>
-              <select className="bg-customPurple-darker input-border" name="" id="">
-                <option className="text-white" value="" disabled>Select</option>
+              <select
+                className="input-border"
+                name=""
+                id=""
+                {...register("groupSize")}
+
+              >
+                <optgroup className="text-sm">
+                  <option value="" selected disabled>
+                    Select
+                  </option>
+                  <option value="" >
+                    Test
+                  </option>
+                </optgroup>
               </select>
+              <p className="text-xs text-red-500">{errors.groupSize?.message}</p> 
+
             </div>
           </div>
           <div className="mt-3">
             <p className="text-customPurple-light text-[9px]">
               <i>Please review your registration details before submitting</i>
             </p>
-            <div className="flex gap-2 mt-3">
-              <input className="w-3 h-3" type="checkbox" />
-              <p className="text-[10px] w-full tracking-[0.025em]">
-              I agreed with the event terms and conditions
-              and privacy policy
-              </p>
+            <div className="flex-col gap-2 mt-3">
+              <div className="flex gap-2">
+                <input 
+                className="w-3 h-3" 
+                type="checkbox"
+                {...register("agree")}
+                />
+                <p className="text-[10px] w-full tracking-[0.025em]">
+                  I agreed with the event terms and conditions and privacy policy
+                </p>
+              </div>
+              <p className="text-xs text-red-500 mt-4">{errors.agree?.message}</p> 
             </div>
           </div>
           <div className="text-center w-full mt-4">
@@ -64,7 +159,7 @@ function Registration() {
         </form>
       </div>
     </div>
-   );
+  );
 }
 
 export default Registration;
