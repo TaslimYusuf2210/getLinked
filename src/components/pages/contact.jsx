a5c897921f63d2919e59fa6075f48808686b9605
Taslim Omotoyosi Yusuf <112773318+TaslimYusuf2210@users.noreply.github.com>
2024-10-07 15:13:47 +0100

    glass form

--- src/components/pages/contact.jsx
+++ src/components/pages/contact.jsx
@@ -2,6 +2,8 @@ import { FaFacebookF } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
+import leftFlare from "../../assets/contactFlare2.svg"
+import rightFlare from "../../assets/contactFlare1.svg"
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -105,33 +107,39 @@ function Contact() {
           </div>
         </div>
       </div>
-      <div className="hidden md:grid md:grid-cols-2 md:min-h-[100vh] md:px-10 py-32">
-        <div className="text-sm grid grid-cols-1 gap-4 h-3/6">
-          <h3 className="text-customPurple font-clashDisplay text-[28px] font-semibold">Get in touch</h3>
-          <p>Contact <br /> Information</p>
-          <p>27,Alara Street <br /> Yaba 100012 <br /> Lagos State</p>
-          <p>Call Us : 07067981819</p>
-          <p>We are open from Monday-Friday <br /> 08:00am - 05:00pm</p>
-          <div className="text-left flex flex-col gap-2">
-            <p className="text-customPurple">Share on</p>
-            <div className="flex gap-2 text-sm">
-              <FaInstagram />
-              <FaXTwitter />
-              <FaFacebookF />
-              <FaLinkedinIn />
+        <img className="absolute opacity-70 left-[175px] top-[20.5%] hidden md:block" src={leftFlare} alt="flare" />
+      <div className="hidden md:grid md:grid-cols-2 md:h-screen md:px-10 py-32">
+        <div className="grid grid-cols-1">
+          <img className="absolute opacity-70 right-[25%] bottom-[10%]" src={rightFlare} alt="flare" />
+          <div className="text-sm grid grid-cols-1 gap-4 h-3/6 mt-10">
+            <h3 className="text-customPurple font-clashDisplay text-[28px] font-semibold">Get in touch</h3>
+            <p>Contact <br /> Information</p>
+            <p>27,Alara Street <br /> Yaba 100012 <br /> Lagos State</p>
+            <p>Call Us : 07067981819</p>
+            <p>We are open from Monday-Friday <br /> 08:00am - 05:00pm</p>
+            <div className="text-left flex flex-col gap-2">
+              <p className="text-customPurple">Share on</p>
+              <div className="flex gap-2 text-sm">
+                <FaInstagram />
+                <FaXTwitter />
+                <FaFacebookF />
+                <FaLinkedinIn />
+              </div>
             </div>
           </div>
         </div>
-        <div>
-          <form className="grid grid-cols-1 gap-6" action="">
-            <header className="text-customPurple font-clashDisplay font-semibold text-xl">
-              Questions or need assistance? <br /> Let us know about it!
-            </header>
-            <input className="input-border px-8" type="text" placeholder="First Name" />
-            <input className="input-border px-8" type="email" placeholder="Mail" />
-            <textarea className="input-border px-8 py-4 h-36" placeholder="Message"></textarea>
-            <div className="text-center">
-              <button className="button w-[172px] h-[53px]">Submit</button>
+        <div className="w-full">
+          <form action="">
+            <div className="grid grid-cols-1 gap-6 p-4 bg-white/10 backdrop-blur-lg shadow-2xl rounded-lg">
+              <header className="text-customPurple font-clashDisplay font-semibold text-xl">
+                Questions or need assistance? <br /> Let us know about it!
+              </header>
+              <input className="input-border px-8 bg-transparent" type="text" placeholder="First Name" />
+              <input className="input-border px-8 bg-transparent" type="email" placeholder="Mail" />
+              <textarea className="input-border px-8 py-4 h-36 bg-transparent" placeholder="Message"></textarea>
+              <div className="text-center">
+                <button className="button w-[172px] h-[53px]">Submit</button>
+              </div>
             </div>
           </form>
         </div>
